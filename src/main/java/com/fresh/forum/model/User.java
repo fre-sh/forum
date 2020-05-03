d2212d1b6ae1b335415d4655bbe71831fdac022f
guowenyu <guowenyu@secsmart.net>
2020-05-03 12:31:54 +0800

    初步

---
+++ src/main/java/com/fresh/forum/model/User.java
@@ -0,0 +1,65 @@
+package com.fresh.forum.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private String salt;
+    private String headUrl;
+
+    public User() {
+
+    }
+
+    public User(String name) {
+        this.name = name;
+        this.password = "";
+        this.salt = "";
+        this.headUrl = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getHeadUrl() {
+        return headUrl;
+    }
+
+    public void setHeadUrl(String headUrl) {
+        this.headUrl = headUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
