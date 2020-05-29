7153d0c625ae481203875e3c9fd4becf836aba83
guowenyu <guowenyu@secsmart.net>
2020-05-29 13:44:39 +0800

    打印返回值

--- src/main/java/com/fresh/forum/controller/BaseController.java
+++ src/main/java/com/fresh/forum/controller/BaseController.java
@@ -1,17 +1,25 @@
 package com.fresh.forum.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fresh.forum.dto.ResponseTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Collection;
+
 public class BaseController {
     protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     /**
      * 捕捉所有未处理的异常
@@ -38,8 +46,14 @@ public class BaseController {
         return ResponseTO.success();
     }
 
-    protected ResponseTO success(Object o) {
-        return ResponseTO.success(o);
+    protected ResponseTO success(Object o){
+        ResponseTO success = ResponseTO.success(o);
+        try {
+            logger.info("response：{}", objectMapper.writeValueAsString(success));
+        } catch (Exception e) {
+            logger.error("" , e);
+        }
+        return success;
     }
 
     protected ResponseTO error(String message) {
